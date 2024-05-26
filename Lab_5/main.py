e8638edb2b25339317b2716bf3786dc9bba43931
FenrirTheLast <147754655+FenrirTheLast@users.noreply.github.com>
2024-05-26 15:02:40 +0200

    lab5

--- Lab_5/main.py
+++ Lab_5/main.py
@@ -73,11 +73,29 @@ print(np.dot(a, b))
 #         print(b[i], end=" ")
 #     print(" ")
 
-a = np.arange(6).reshape((3,2))
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# print(a.shape)
+# print(type(a.shape))
+# for i in range(0, a.shape[0]):
+#     for j in range(0, a.shape[1]):
+#         print(a[i][j], end=' ')
+#     print(' ')
+a = np.arange(6)
 print(a)
 print(a.shape)
-print(type(a.shape))
-for i in range(0, a.shape[0]):
-    for j in range(0, a.shape[1]):
-        print(a[i][j], end=' ')
-    print(' ')
+print("")
+b = a.reshape((2,3))
+print(b)
+print(b.shape)
+print("")
+c = b.reshape((3,2))
+print(c)
+print(c.shape)
+print("")
+d = c.ravel()
+print(d)
+print(d.shape)
+print("")
+
+
